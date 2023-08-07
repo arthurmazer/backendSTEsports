95cca5c7ddb53adf8bdec4c05df980eccaebedb2
Arthur Mazer <arthurmazer@ciandt.com>
2023-08-07 13:11:44 -0300

    first commit

---
+++ src/main/kotlin/com/mazer/est/ESocioTorcedorApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mazer.est
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ESocioTorcedorApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ESocioTorcedorApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mazer/est/ESocioTorcedorApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mazer.est
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ESocioTorcedorApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
